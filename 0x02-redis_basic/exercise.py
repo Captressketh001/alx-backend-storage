ceae7365c08aa84c3e08208d2d76570340c9aa56
Captressketh001 <shininglite2012@gmail.com>
2023-07-22 21:45:54 +0000

    storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,11 +2,38 @@
 """
 Writing strings to Redis
 """
+from functools import wraps
 import redis
 from uuid import uuid4
 from typing import Union, Callable, Optional
 
+def call_history(method: Callable) -> Callable:
+    key = method.__qualname__
+    inputs = key + ':inputs'
+    outputs = key + ':outputs'
 
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
+
+    
 class Cache:
     """Represents a class Cache"""
 
@@ -14,6 +41,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Create a store method that takes a
         data argument and returns a string"""
